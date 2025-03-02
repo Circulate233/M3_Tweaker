0d159da6f9c78f0026aa32aaf4216fd27a8544ff
Circulate233 <a18966009058@163.com>
2025-03-02 21:55:29 +0800

    允许M3自定义饰品使用nbt定义需求的等级

---
+++ src/main/java/com/circulation/m3t/mixins/MMM/MixinMagicItemOP.java
@@ -0,0 +1,26 @@
+package com.circulation.m3t.mixins.MMM;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import project.studio.manametalmod.optool.MagicItemOP;
+
+@Mixin(value = MagicItemOP.class,remap = false)
+public class MixinMagicItemOP {
+
+    /**
+     * @author circulation
+     * @reason 允许M3自定义饰品使用nbt定义需求的等级
+     */
+    @Overwrite
+    public int getNeedLV(ItemStack item, EntityPlayer player) {
+        if (item.hasTagCompound()){
+            if (item.getTagCompound().hasKey("level")){
+                return Math.min(Math.max(item.getTagCompound().getInteger("level"),1),130);
+            }
+        }
+        return 1;
+    }
+
+}
